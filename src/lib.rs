849ead7eb5d51b615c71b292e4d6e72c38ddd5ee
Danny Willems <be.danny.willems@gmail.com>
2024-01-26 22:58:48 +0100

    Update add

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,32 @@ pub struct S1 {
     pub y: u64,
 }
 
+pub struct S2 {
+    pub x: u64,
+    pub y: u64,
+    pub z: u64,
+}
+
+// Old version
+// impl Add for S1 {
+//     type Output = S1;
+
+//     fn add(self, rhs: Self) -> Self::Output {
+//         S1 {
+//             x: self.x + rhs.x,
+//             y: self.y + rhs.y,
+//         }
+//     }
+// }
+
 impl Add for S1 {
-    type Output = S1;
+    type Output = S2;
 
     fn add(self, rhs: Self) -> Self::Output {
-        S1 {
+        S2 {
             x: self.x + rhs.x,
             y: self.y + rhs.y,
+            z: self.x + rhs.y,
         }
     }
 }
