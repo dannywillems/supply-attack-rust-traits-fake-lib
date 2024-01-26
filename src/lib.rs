f0eb06b5e3644b0e12ba19fbbe83cafdd88869b6
Danny Willems <be.danny.willems@gmail.com>
2024-01-26 22:41:30 +0100

    First

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+use std::ops::Add;
+
+struct S1 {
+    pub x: u64,
+    pub y: u64,
+}
+
+impl Add for S1 {
+    type Output = S1;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        S1 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
